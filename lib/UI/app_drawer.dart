c90e7cef6686d0db7de76a09572e9a88225d38bf
Nirvana <shoaibarms@gmail.com>
2024-03-17 13:34:20 +0530

    Initial commit

---
+++ lib/UI/app_drawer.dart
@@ -0,0 +1,82 @@
+import 'package:device_apps/device_apps.dart';
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatefulWidget {
+  final List sortedApps;
+  final List sortApps;
+  final List applications;
+  const AppDrawer({super.key, required this.sortedApps, required this.sortApps, required this.applications});
+
+  @override
+  State<AppDrawer> createState() => _AppDrawerState();
+}
+
+class _AppDrawerState extends State<AppDrawer> {
+  List sortedApps = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    sortedApps = widget.sortApps;
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Flexible(
+          // height: MediaQuery.of(context).size.height * 0.60,
+          child: GridView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: widget.applications.length,
+          itemBuilder: (context, index) {
+            // final applicationIcon = widget.sortedApps.isNotEmpty ? widget.sortedApps[index].icon as Uint8List : widget.sortApps[index].icon as Uint8List;
+          final application = widget.sortedApps.isNotEmpty
+              ? sortedApps[index] as ApplicationWithIcon
+              : widget.sortApps[index] as ApplicationWithIcon;
+          return SizedBox(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () => DeviceApps.openApp(application.packageName),
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      child: AspectRatio(
+                        aspectRatio: 6 / 6,
+                        child: Container(
+                            // width: MediaQuery.of(context).size.width * 0.19,
+                            // height: MediaQuery.of(context).size.height * 0.28,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                  10.0,
+                                ),
+                                color: Colors.white),
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.memory(application.icon))),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * 0.04,
+                    child: Text(
+                      application.appName,
+                      style: const TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                      maxLines: 3,
+                      overflow: TextOverflow.clip,
+                    ),
+                  )
+                ]),
+          );
+          },
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            mainAxisSpacing: 10, maxCrossAxisExtent: 100, crossAxisSpacing: 10),
+            ),
+        );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:device_apps/device_apps.dart';
+import 'package:flutter/material.dart';
+import 'package:s_launcher/UI/app_drawer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'S Launcher',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+ 
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<Application> applications = [];
+  bool loading = false;
+  void getApplications() async {
+    try {
+      loading = true;
+      final apps = await DeviceApps.getInstalledApplications(
+        includeAppIcons: true,
+        includeSystemApps: true,
+        onlyAppsWithLaunchIntent: true,
+      );
+      setState(() {
+        applications = apps;
+      });
+      loading = false;
+    } catch (e) {
+      SnackBar(
+        content: Text(e.toString()),
+      );
+    } finally {
+      loading = false;
+    }
+  }
+
+  List sortedApps = [];
+
+  List sortApps(List appsListName) {
+    sortedApps = appsListName.map((e) => e).toList();
+    sortedApps.sort(
+      (a, b) => a.appName
+          .toString()
+          .toLowerCase()
+          .compareTo(b.appName.toString().toLowerCase()),
+    );
+    return sortedApps;
+  }
+ 
+
+  @override
+  void initState() {
+    super.initState();
+    getApplications();
+    if (sortedApps.isNotEmpty) {
+      sortApps(applications);
+    }
+  }
+
+   @override
+  void dispose() {
+    // TODO: implement dispose
+    getApplications();
+    sortApps(applications);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body:  SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+             const Text('Hi Shoaib! :)', style:  TextStyle(fontSize: 30, color: Colors.white),),
+              const SizedBox(height: 10.0,),
+              loading
+                  ? const Expanded(
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    )
+                  : AppDrawer(sortedApps:sortedApps, sortApps: sortApps(applications), applications: applications,)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
